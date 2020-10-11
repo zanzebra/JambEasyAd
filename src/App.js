ee64bdbdf0102cecfcff044fd72b9a80e0a25868
zanzebra <zanzebra101@gmail.com>
2020-10-11 20:18:55 +0100

    designed cursor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { LandingPage } from "./components/LandingPage/LandingPage";
+import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = React.useState(false);
+  React.useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+    document.addEventListener("mousemove", (e) => {
+      cursor.setAttribute(
+        "style",
+        "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px"
+      );
+    });
+    document.addEventListener("click", () => {
+      cursor.classList.add("expand");
+      setTimeout(() => {
+        cursor.classList.remove("expand");
+      }, 500);
+    });
+  });
   return (
-    <Switch>
-      <Route component={LandingPage} />
-    </Switch>
+    <>
+      <div className="cursor"></div>
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+      <Switch>
+        <Route path="/">
+          <LandingPage setShowModal={setShowModal} />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/LandingPage/paths/pathVariants.js
@@ -0,0 +1,135 @@
+export const pathVariants1 = {
+  hidden: {
+    opacity: 0,
+    fill: "none",
+    stroke: "none",
+    strokeWidth: "8px",
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "none",
+    stroke: "#fff",
+    strokeWidth: "8px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+  visible2: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "none",
+    stroke: "#fff",
+    strokeWidth: "8px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export const pathVariants2 = {
+  hidden: {
+    opacity: 0,
+    fill: "none",
+    stroke: "none",
+    strokeWidth: "8px",
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "none",
+    stroke: "#fff",
+
+    strokeWidth: "8px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+  visible2: {
+    opacity: 1,
+    pathLength: 0,
+    fill: "none",
+    stroke: "#fff",
+    strokeWidth: "8px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export const pathVariants3 = {
+  visible: {
+    scale: [0, 1],
+    transition: {
+      duration: 0.5,
+      yoyo: Infinity,
+      ease: "easeInOut",
+    },
+  },
+  visible2: {
+    scale: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export const pathVariants5 = {
+  hidden: {
+    opacity: 0,
+    fill: "none",
+    stroke: "none",
+    strokeWidth: "1px",
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "none",
+    stroke: "#fff",
+    strokeWidth: "1px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+  visible2: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "none",
+    stroke: "#fff",
+    strokeWidth: "8px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+export const pathVariants4 = {
+  hidden: {
+    opacity: 0,
+    fill: "none",
+    stroke: "none",
+    strokeWidth: "2px",
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "none",
+    stroke: "#fff",
+    strokeWidth: "2px",
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
